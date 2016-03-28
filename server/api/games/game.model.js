ee6916b1aae104b2d7904a285140c1e4ecc56ff6
Chris Sakai <cs.chris.sakai@gmail.com>
2016-03-28 13:25:27 -0400

    Add game schema. Add index for game namespace.

---
+++ server/api/games/game.model.js
@@ -0,0 +1,66 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var _ = require('lodash');
+
+var RowSchema = new Schema({
+    a: { type: String, default: ''},
+    b: { type: String, default: ''},
+    c: { type: String, default: ''},
+    d: { type: String, default: ''},
+    e: { type: String, default: ''}
+});
+
+var BoardSchema = new Schema({
+    a: RowSchema,
+    b: RowSchema,
+    c: RowSchema,
+    d: RowSchema,
+    e: RowSchema
+});
+
+var GameSchema = new Schema({
+    playerBoard: BoardSchema,
+    cpuBoard: BoardSchema,
+    active: { type: Boolean, default: true },
+    win: Boolean
+});
+
+/**
+ * Virtuals
+ */
+function coordinateMapper(coords) {
+    return coords.split('').join('.');
+}
+
+GameSchema
+    .virtual('move')
+    .set(function(coordinates) {
+        var val,
+            newVal,
+            path = 'cpuBoard.';
+        path += coordinateMapper(coordinates)
+        //Will be "S" for ship or '' for nothing
+        val = _.get(this, path);
+        if (val === 'S') {
+            //HIT!
+            newVal = 'H';
+        }  else {
+            //MISS!
+            newVal = 'M';
+        }
+        _.set(this, path, newVal);
+    });
+
+function placeShip(coord) {
+    _.set(this, (this._path + coord), 'S');
+}
+
+GameSchema
+    .virtual('board')
+    .set(function(coordObj) {
+        var coordArr = _.map(coordObj.coordinates, coordinateMapper);
+        this._path = coordObj.name + '.';
+        coordArr.forEach(placeShip, this);
+    });
+
+module.exports = mongoose.model('User', GameSchema);
---
+++ server/api/games/index.js
@@ -0,0 +1,73 @@
+var GameCtrl = require('./game.controller');
+var express = require('express');
+var router = express.Router();
+
+router
+    .put('/', function(req, res, next) {
+        var ctrl = new GameCtrl();
+        ctrl.newGame()
+            .then(function(payload) {
+                res.status(201).json(payload);
+            }).catch(next);
+    });
+
+router
+    .route('/:id')
+    .get(function(req, res, next) {
+        var id = req.params.id;
+        var ctrl = new GameCtrl(id);
+        ctrl.getGame()
+            .then(function(payload) {
+                res.status(200).json(payload);
+            }).catch(next);
+    })
+    .delete(function(req, res, next) {
+        var id = req.params.id;
+        var ctrl = new GameCtrl(id);
+        ctrl.endGame()
+            .then(function(payload) {
+                res.status(200).json(payload);
+            }).catch(next);
+    });
+
+router
+    .route('/:id/ships')
+    .post(function(req, res, next) {
+        var id = req.params.id;
+        var ctrl = new GameCtrl(id);
+        var coords = req.query.coords;
+        ctrl.placeShips(coords)
+            .then(function(payload) {
+                res.status(201).json(payload);
+            }).catch(next);
+    })
+    .get(function(req, res, next) {
+        var id = req.params.id;
+        var ctrl = new GameCtrl(id);
+        ctrl.getShips()
+            .then(function(payload) {
+                res.status(200).json(payload);
+            }).catch(next);
+    });
+
+router
+    .route('/:id/move')
+    .post(function(req, res, next) {
+        var id = req.params.id;
+        var ctrl = new GameCtrl(id);
+        var coord = req.body.coord;
+        ctrl.move(coord)
+            .then(function(payload) {
+                res.status(200).json(payload);
+            }).catch(next);
+    })
+    .get(function(req, res, next) {
+        var id = req.params.id;
+        var ctrl = new GameCtrl(id);
+        ctr.getMove()
+            .then(function(payload) {
+                res.status(200).json(payload);
+            }).catch(next);
+    });
+
+module.exports = router;
