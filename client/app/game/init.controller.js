e672a881e1e7c83ff00586f953ab04e566b3840d
Chris Sakai <cs.chris.sakai@gmail.com>
2016-03-30 16:41:37 -0400

    Add init controller

---
+++ client/app/game/init.controller.js
@@ -0,0 +1,48 @@
+(function() {
+    var app = angular.module('battleship');
+    app.controller('initCtrl', ['$q', '$scope', 'gameService', initCtrl]);
+    function initCtrl($q, $scope, gameService) {
+        var coordNames = ['a','b','c','d','e'];
+        $scope.cpuBoard = [
+            ['', '', '', '', ''],
+            ['', '', '', '', ''],
+            ['', '', '', '', ''],
+            ['', '', '', '', ''],
+            ['', '', '', '', '']
+        ];
+        $scope.playerBoard = [
+            ['', '', '', '', ''],
+            ['', '', '', '', ''],
+            ['', '', '', '', ''],
+            ['', '', '', '', ''],
+            ['', '', '', '', '']
+        ];
+        $scope.activeGame = false;
+        function _setBoard(data) {
+            _.forEach(coordNames, function (key1, index1) {
+                _.forEach(coordNames, function(key2, index2) {
+                    $scope.cpuBoard[index2][index1] = data.cpuBoard[key2][key1];
+                    $scope.playerBoard[index2][index1] = data.cpuBoard[key2][key1];
+                });
+            });
+            return;
+        }
+        $scope.getGame = function(gameData) {
+            var func;
+            var param;
+            if (gameData) {
+                func = $q.resolve;
+                param = gameData;
+            } else {
+                func = gameService.getGame;
+                param = $scope.gameId;
+            }
+            func(param)
+                .then(function(data) {
+                    $scope.id = $scope.gameId;
+                    $scope.activeGame = true;
+                    return data;
+                }).then(_setBoard);
+        };
+    }
+})();
