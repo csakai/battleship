ad12214d7999218820ff28f9ae553dcec2e53ff5
Chris Sakai <cs.chris.sakai@gmail.com>
2016-03-30 22:13:27 -0400

    Change update document to be passed back to the client from game controller. Add data massaging method for moves

--- server/api/games/game.controller.js
+++ server/api/games/game.controller.js
@@ -6,7 +6,12 @@ function GameCtrl(id) {
     if (id) {
         this.id = id;
     }
+    this.moveProps = ['active', 'col', 'playerTurn', 'result', 'row', 'win'];
 }
+
+GameCtrl.prototype.returnMoveData = function returnMoveData() {
+    return _.pick(this, this.moveProps);
+};
 
 GameCtrl.prototype.index = function index(query) {
     return Game.find(query, '-cpuBoard -playerBoard');
@@ -31,33 +36,47 @@ GameCtrl.prototype.placeShips = function placeShips(coords) {
         }).then(function(data) {
             return data.setCpuBoard(util.randomCoords());
         }).then(function(data) {
-            return data.save();
+            return data
+                .update(data)
+                .then(function() {
+                    return data;
+                });
         });
 };
 
 GameCtrl.prototype.move = function move(coord) {
+    this.coords = coord;
+    var self = this;
     return Game.findById(this.id)
         .then(function(data) {
             return data.playerMove(coord);
-        }).then(this.applyMoveFn('cpu'));
+        }).then(this.applyMoveFn('cpu'))
+        .then(this.returnMoveData);
 };
 
 GameCtrl.prototype.cpuMove = function getMove() {
+    var self = this;
     return Game.findById(this.id)
         .then(function(data) {
-            return data.cpuMove(util.randomCoords(data.playerBoard));
-        }).then(this.applyMoveFn('player'));
+            var coords = util.randomCoords(data.playerBoard);
+            self.coords = coords[0]
+            return data.cpuMove(coords);
+        }).then(this.applyMoveFn('player'))
+        .then(this.returnMoveData);
 };
 
 GameCtrl.prototype.endGame = function endGame() {
     var self = this;
     return Game.findById(this.id)
-        .then(this.applyMoveFn());
+        .then(this.applyMoveFn())
+        .then(this.returnMoveData);
 };
 
 GameCtrl.prototype.applyMoveFn = function applyMove(name) {
+    var self = this;
     return function applyMove(data) {
         var updateDoc = {};
+        self.playerTurn = data.playerTurn;
         if (!name) {
             updateDoc.active = false;
             updateDoc.win = false;
@@ -66,7 +85,13 @@ GameCtrl.prototype.applyMoveFn = function applyMove(name) {
             updateDoc.win = ('cpu' === name);
         }
         _.assign(data, updateDoc);
-        return data;
+        _.assign(self, updateDoc);
+        if (self.coords) {
+            self.row = self.coords.charAt(0);
+            self.col = self.coords.charAt(1);
+            self.result = util.getResult(data, name, self.coords);
+        }
+        return data.save();
     }
 }
 
